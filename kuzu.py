53972f60fe430d80c8bf343aad7e14475a4e50b5
TrinaChang <ks20342@gmail.com>
2021-10-23 21:43:24 +0800

    finished p1

--- kuzu.py
+++ kuzu.py
@@ -15,23 +15,35 @@ class NetLin(nn.Module):
         self.lin = nn.Linear(28 * 28, 10)
 
     def forward(self, x):
-        return F.log_softmax(self.lin(x.view(x.size(0), -1)))
+        return F.log_softmax(self.lin(x.view(x.size(0), -1)), dim=1)
 
 class NetFull(nn.Module):
     # two fully connected tanh layers followed by log softmax
     def __init__(self):
         super(NetFull, self).__init__()
-        # INSERT CODE HERE
+        self.in_to_hid = nn.Linear(28 * 28, 250)
+        self.hid_to_out = nn.Linear(250, 10)
 
     def forward(self, x):
-        return 0 # CHANGE CODE HERE
+        x = torch.tanh(self.in_to_hid(x.view(x.size(0), -1)))
+        return F.log_softmax(self.hid_to_out(x), dim=1)
 
 class NetConv(nn.Module):
     # two convolutional layers and one fully connected layer,
     # all using relu, followed by log_softmax
     def __init__(self):
         super(NetConv, self).__init__()
-        # INSERT CODE HERE
+        self.conv1 = nn.Conv2d(1, 64, 5)
+        self.conv2 =  nn.Conv2d(64, 32, 5)
+        self.lin1 = nn.Linear(512, 256)
+        self.lin2 = nn.Linear(256, 10)
 
     def forward(self, x):
-        return 0 # CHANGE CODE HERE
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.lin1(out))
+        out = F.log_softmax(self.lin2(out), dim=1)
+        return out 
